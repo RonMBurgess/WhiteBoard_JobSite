198cba0b81d39348bc4f1e2dd959236f8789ff54
Oceanh2 <kevin.h.martindale@gmail.com>
2014-03-28 12:54:46 -0400

    fixing duplicate salary entry

--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -513,36 +513,36 @@ function twentyfourteen_wp_title( $title, $sep ) {
 }
 
 // Salary field for main form
-add_filter('submit_job_form_fields', 'frontend_add_salary_field');
+//add_filter('submit_job_form_fields', 'frontend_add_salary_field');
 
-function frontend_add_salary_field($fields) {
-	$fields['job']['job_salary'] = array(
-		'label' => __('Salary', 'job_manager'),
-		'type' => 'text',
-		'required' => true,
-		'placeholder' => '',
-		'priority' => 7
-	);
-	return $fields;
-}
+//function frontend_add_salary_field($fields) {
+//	$fields['job']['job_salary'] = array(
+//		'label' => __('Salary', 'job_manager'),
+//		'type' => 'text',
+//		'required' => true,
+//		'placeholder' => '',
+//		'priority' => 7
+//	);
+//	return $fields;
+//}
 
-add_action('job_manager_update_job_data', 'frontend_add_salary_field_save', 10, 2);
+//add_action('job_manager_update_job_data', 'frontend_add_salary_field_save', 10, 2);
 
-function frontend_add_salary_field_save($job_id, $values) {
-	update_post_meta($job_id, '_job_salary', $values['jpb']['job_salary']);
-}
+//function frontend_add_salary_field_save($job_id, $values) {
+//	update_post_meta($job_id, '_job_salary', $values['jpb']['job_salary']);
+//}
 
-add_filter( 'job_manager_job_listing_data_fields', 'admin_add_salary_field' );
+//add_filter( 'job_manager_job_listing_data_fields', 'admin_add_salary_field' );
 
-function admin_add_salary_field($fields) {
-	$fields['_job_salary'] = array(
-		'label' => __('Salary', 'job_manager'),
-		'type' => 'tect',
-		'placeholder' => '',
-		'description' => ''
-	);
-	return $fields;
-}
+//function admin_add_salary_field($fields) {
+//	$fields['_job_salary'] = array(
+//		'label' => __('Salary', 'job_manager'),
+//		'type' => 'tect',
+//		'placeholder' => '',
+//		'description' => ''
+//	);
+//	return $fields;
+//}
 
 add_filter( 'wp_title', 'twentyfourteen_wp_title', 10, 2 );
 
